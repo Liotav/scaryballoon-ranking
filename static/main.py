b82d9ce31fa1f5df648e74f9d27ea7ca2b79ad20
Liotav <tavares321@hotmail.com>
2025-06-02 23:02:33 -0400

    Update main.py

--- static/main.py
+++ static/main.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-# Página inicial já carrega o index.html
-@app.route("/game")
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/game")
+def game():
     return render_template("game.html")
 
-# Servir arquivos de imagem, som, etc.
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory('static', filename)
